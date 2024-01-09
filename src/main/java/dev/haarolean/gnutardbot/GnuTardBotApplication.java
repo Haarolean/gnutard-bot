e43c2552b9b2d0d8287a305a3a304b6d5abad47d
Roman Zabaluev <gpg@haarolean.dev>
2024-01-09 07:41:20 +0700

    Initial commit

---
+++ src/main/java/dev/haarolean/gnutardbot/GnuTardBotApplication.java
@@ -0,0 +1,13 @@
+package dev.haarolean.gnutardbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnuTardBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GnuTardBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/haarolean/gnutardbot/GnuTardBotApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.haarolean.gnutardbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GnuTardBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
